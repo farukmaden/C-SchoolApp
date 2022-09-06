ace4d84eb219879c5f64b6c5242be0a991133174
Faruk Maden <muhammedfrkmdn@gmail.com>
2022-09-06 15:09:18 +0300

    Create StudentModel.cs

---
+++ AdoNetExample/AdoNetExample/Models/UserModels/StudentModel.cs
@@ -0,0 +1,19 @@
+﻿using AdoNetExample.Enums;
+using AdoNetExample.Models.BaseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetExample.Models.UserModels
+{
+    public class StudentModel : BaseModel
+    {
+        public StudentModel()
+        {
+
+        }
+        public UserTypeEnum UserTypeEnum { get; set; }
+    }
+}
