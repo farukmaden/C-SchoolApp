13fe9d25752d47eed89e6f364929ada2d01ac91a
Faruk Maden <muhammedfrkmdn@gmail.com>
2022-09-06 15:09:09 +0300

    Create IDataAccess.cs

---
+++ AdoNetExample/AdoNetExample/IDataAccess.cs
@@ -0,0 +1,20 @@
+﻿using AdoNetExample.Models.UserModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetExample
+{
+    public interface IDataAccess
+    {
+        void add(StudentModel studentModel);
+        void delete(int selectedId);
+        void update(int selectedId, StudentModel studentModel);
+        void getAll();
+        void getById(int selectedId);
+        void addColumn();
+        void getByUserType(int selectedType);
+    }
+}
