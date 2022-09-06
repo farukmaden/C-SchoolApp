f43880e034fafcd4fbd882c3cce5e2143dc0e797
Faruk Maden <muhammedfrkmdn@gmail.com>
2022-09-06 15:09:12 +0300

    Create BaseModel.cs

---
+++ AdoNetExample/AdoNetExample/Models/BaseModels/BaseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetExample.Models.BaseModels
+{
+    public class BaseModel : IBaseModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
